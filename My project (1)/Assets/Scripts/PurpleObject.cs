9092fadbc3d6c7602fef6ca09692ae51e5e4fbfa
ElizaPush <liza.ponomarenko.0677@gmail.com>
2025-12-02 23:20:02 +0300

    animation work

--- My project (1)/Assets/Scripts/PurpleObject.cs
+++ My project (1)/Assets/Scripts/PurpleObject.cs
@@ -2,29 +2,23 @@ using UnityEngine;
 
 public class PurpleObject : InteractableObjects
 {
-    [SerializeField] private Renderer _renderer;
     private Animator _animator;
 
     private void OnEnable()
     {
         _animator = GetComponent<Animator>();
     }
+
     protected override void SetState(State state)
     {
         base.SetState(state);
         switch (state)
         {
-            case State.Active:
-                _animator.SetTrigger("StartInteraction");
-                if (_renderer == null) return;
-                _renderer.materials[0].EnableKeyword("_EMISSION");
-                _renderer.materials[0].SetColor("EmissionColor", new Color(0.5f, 0.5f, 0.5f, 0.1f));
-                break;
             case State.Idle:
                 _animator.SetTrigger("DoToIdle");
-                if (_renderer == null) return;
-                _renderer.materials[0].EnableKeyword("_EMISSION");
-                _renderer.materials[0].SetColor("EmissionColor", new Color(0, 0, 0, 0));
+                break;
+            case State.Active:
+                _animator.SetTrigger("StartInteraction");
                 break;
         }
     }
